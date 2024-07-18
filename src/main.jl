50887225f9d0e9f9e5a4ed58293b554b4d99a937
Sven Duve <svenduve@gmail.com>
2024-07-18 16:13:53 +0200

    added Floats

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,12 @@
 module main
 
-export addtwoonumbers
+export addtwonumbers
 
-function addtwoonumbers(a::Int, b::Int)
+function addtwonumbers(a::Int, b::Int)
+    a + b
+end
+
+function addtwonumbers(a::Float64, b::Float64)
     a + b
 end
 
