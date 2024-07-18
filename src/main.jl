cb1f5d5370ea21f231b2385351a5ef84e1a0998c
Sven Duve <svenduve@gmail.com>
2024-07-18 16:23:50 +0200

    added subtraction

--- src/main.jl
+++ src/main.jl
@@ -1,9 +1,13 @@
 module main
 
-export addtwonumbers
+export addtwonumbers, subtracttwonumbers
 
 function addtwonumbers(a::Int, b::Int)
     a + b
+end
+
+function subtracttwonumbers(a::Int, b::Int)
+    a - b
 end
 
 end
